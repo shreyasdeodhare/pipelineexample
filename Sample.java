b917e5ec8762368a0baad12fa82e6eb091a62543
Shreyas Deodhare <67466611+shreyasdeodhare@users.noreply.github.com>
2024-01-22 17:32:51 +0530

    Initial Commit

---
+++ Sample.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*; 
+class Sample{
+
+    public static void main(String[] args) {
+        String s="YEs";
+        String s1="No"; 
+
+        System.out.println(s+" "+s1);
+    }
+}
+
